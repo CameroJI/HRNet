bd6ff1d59079ed01aa46a10e8c1b534bea5bf78d
CameroJI <137007386+CameroJI@users.noreply.github.com>
2023-08-27 23:20:56 -0700

    Update cityscapes.py

--- lib/datasets/cityscapes.py
+++ lib/datasets/cityscapes.py
@@ -47,23 +47,17 @@ class Cityscapes(BaseDataset):
         if num_samples:
             self.files = self.files[:num_samples]
 
-        self.label_mapping = {-1: ignore_label, 0: ignore_label, 
-                              1: ignore_label, 2: ignore_label, 
-                              3: ignore_label, 4: ignore_label, 
-                              5: ignore_label, 6: ignore_label, 
-                              7: 0, 8: 1, 9: ignore_label, 
-                              10: ignore_label, 11: 2, 12: 3, 
-                              13: 4, 14: ignore_label, 15: ignore_label, 
-                              16: ignore_label, 17: 5, 18: ignore_label, 
-                              19: 6, 20: 7, 21: 8, 22: 9, 23: 10, 24: 11,
-                              25: 12, 26: 13, 27: 14, 28: 15, 
-                              29: ignore_label, 30: ignore_label, 
-                              31: 16, 32: 17, 33: 18}
+        self.label_mapping = {0: ignore_label, 
+                              1: 0, 2: 1, 
+                              3: 2, 4: 3, 
+                              5: 4, 6: 5, 
+                              7: 6, 8: 7, 9: 8, 
+                              10: 9, 11: 10, 12: 11, 13: 12}
+                     
         self.class_weights = torch.FloatTensor([0.8373, 0.918, 0.866, 1.0345, 
                                         1.0166, 0.9969, 0.9754, 1.0489,
                                         0.8786, 1.0023, 0.9539, 0.9843, 
-                                        1.1116, 0.9037, 1.0865, 1.0955, 
-                                        1.0865, 1.1529, 1.0507]).cuda()
+                                        1.1116]).cuda()
     
     def read_files(self):
         files = []
