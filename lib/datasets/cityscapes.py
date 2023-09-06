54ad4d448de1d447a98bc82404b9e89941187203
CameroJI <137007386+CameroJI@users.noreply.github.com>
2023-09-05 23:43:22 -0700

    Update cityscapes.py

--- lib/datasets/cityscapes.py
+++ lib/datasets/cityscapes.py
@@ -47,12 +47,12 @@ class Cityscapes(BaseDataset):
         if num_samples:
             self.files = self.files[:num_samples]
 
-        self.label_mapping = {0: ignore_label, 
-                              1: 0, 2: 1, 
-                              3: 2, 4: 3, 
-                              5: 4, 6: 5, 
-                              7: 6, 8: 7, 9: 8, 
-                              10: 9, 11: 10, 12: 11}
+        self.label_mapping = {0: 0,#ignore_label, 
+                              1: 1, 2: 2, 
+                              3: 3, 4: 4, 
+                              5: 5, 6: 6, 
+                              7: 7, 8: 8, 9: 9, 
+                              10: 10, 11: 11, 12: 12}
                      
         self.class_weights = torch.FloatTensor([0.8373, 0.918, 0.866, 1.0345, 
                                         1.0166, 0.9969, 0.9754, 1.0489,
